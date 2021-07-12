479dbed916c9bfc4a4c7ad8287e3ea77d09fd5b0
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-07-12 14:57:36 +0900

    Feat : Update Header

---
+++ navigation/Tabs.js
@@ -0,0 +1,26 @@
+import React, { useLayoutEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+import Favorite from "../screens/Favorite";
+
+const Tabs = createBottomTabNavigator();
+
+const getHeaderName = (route) =>
+  getFocusedRouteNameFromRoute(route) || "Movies";
+
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: getHeaderName(route) });
+  }, [route]);
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Movies" component={Movies} />
+      <Tabs.Screen name="Tv" component={Tv} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favorite" component={Favorite} />
+    </Tabs.Navigator>
+  );
+};
--- screens/Movies.js
+++ screens/Movies.js
@@ -2,10 +2,9 @@ import React from "react";
 import { Button, Text, View } from "react-native";
 
 export default ({ navigation }) => {
-  console.log(navigation);
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Movies</Text>
       <Button title="상세보기" onPress={() => navigation.navigate("Detail")} />
     </View>
   );

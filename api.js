d7b9a56eac2075ee782a9bc7dfc3900128916f99
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-07-14 15:50:45 +0900

    Feat : Add getData of nowPlaying and popular

---
+++ api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const TMDB_KEY = "315189377c6e8a3d7ada3cb66d4765c5";
+
+const makeRequest = (path, params) =>
+  axios.get(`https://api.themoviedb.org/3${path}`, {
+    params: {
+      ...params,
+      api_key: TMDB_KEY,
+    },
+  });
+
+const getAnything = async (path, params = {}) => {
+  try {
+    const {
+      data: { results },
+    } = await makeRequest(path, params);
+    return [results, null];
+  } catch (error) {
+    return [null, error];
+  }
+};
+
+export const movieApi = {
+  nowPlaying: () => getAnything("/movie/now_playing"),
+  popular: () => getAnything("/movie/popular"),
+  upcoming: () => getAnything("/movie/upcoming", { region: "kr" }),
+  search: (query) => getAnything("/search/movie", { query }),
+  movie: (id) => getAnything(`/movie/${id}`),
+  discover: () => getAnything("/discover/movie"),
+};
+
+export const tvApi = {
+  today: () => getAnything("/tv/airing_today"),
+  thisWeek: () => getAnything("/tv/on_the_air"),
+  topRated: () => getAnything("/tv/top_rated"),
+  popular: () => getAnything("/tv/popular"),
+  search: (query) => getAnything("/search/tv", { query }),
+  show: (id) => getAnything(`/tv/${id}`),
+};
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Text, View } from "react-native";
+import { movieApi } from "../api";
 
-export default ({ navigation }) => {
+export default () => {
+  const getData = async () => {
+    const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+    const [popular, popularError] = await movieApi.popular();
+    console.log(popular);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
       <Text>Movies</Text>
-      <Button title="상세보기" onPress={() => navigation.navigate("Detail")} />
     </View>
   );
 };

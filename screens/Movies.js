60dc358dabe33643cb5a38aa8325ce929456e8cd
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-07-14 17:24:24 +0900

    Feat : Getting movies

--- screens/Movies.js
+++ screens/Movies.js
@@ -5,17 +5,33 @@ import { Text, View } from "react-native";
 import { movieApi } from "../api";
 
 export default () => {
+  const [movies, setMovies] = useState({
+    nowPlaying: [],
+    popular: [],
+    upcoming: [],
+    nowPlayingError: null,
+    popularError: null,
+    upcomingError: null,
+  });
   const getData = async () => {
     const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
     const [popular, popularError] = await movieApi.popular();
-    console.log(popular);
+    const [upcoming, upcomingError] = await movieApi.upcoming();
+    setMovies({
+      nowPlaying,
+      popular,
+      upcoming,
+      nowPlayingError,
+      popularError,
+      upcomingError,
+    });
   };
   useEffect(() => {
     getData();
   }, []);
   return (
     <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Text>Movies</Text>
+      <Text style={{ color: "white" }}>{movies.nowPlaying?.length}</Text>
     </View>
   );
 };

ca656d41381338e2a51d3c341b3e98bc38a40e2e
SeungsooChoi <tmdtn5389@gmail.com>
2021-07-19 20:09:25 +0900

    Feat : slider finished

--- components/Movies/Slide.js
+++ components/Movies/Slide.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
 import { apiImage } from "../../api";
+import Poster from "../Poster";
+import { TouchableOpacity } from "react-native";
 
 const Container = styled.View`
   width: 100%;
@@ -11,42 +13,66 @@ const Container = styled.View`
 const BG = styled.Image`
   height: 100%;
   width: 100%;
-  opacity: 0.8;
+  opacity: 0.3;
   position: absolute;
 `;
 
 const Content = styled.View`
+  height: 100%;
   flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const Data = styled.View`
   width: 50%;
+  align-items: flex-start;
 `;
 
 const Title = styled.Text`
   color: white;
   font-weight: bold;
   font-size: 18px;
+  margin-bottom: 12px;
 `;
 
 const Votes = styled.Text`
-  color: white;
-  opacity: 0.7;
+  color: rgb(210, 210, 210);
+  margin-bottom: 10px;
 `;
 
 const Overview = styled.Text`
-  color: white;
-  opacity: 0.7;
+  color: rgb(210, 210, 210);
+  font-weight: 500;
 `;
 
-const Slide = ({ id, title, backgroundImage, votes, overview }) => (
+const Button = styled.View`
+  background-color: white;
+  padding: 4px 8px;
+  margin-top: 4px;
+  border-radius: 2px;
+`;
+
+const ButtonText = styled.Text`
+  color: black;
+`;
+
+const Slide = ({ id, title, backgroundImage, votes, overview, poster }) => (
   <Container>
     <BG source={{ uri: apiImage(backgroundImage) }} />
     <Content>
+      <Poster url={apiImage(poster)} />
       <Data>
         <Title>{title}</Title>
-        <Votes>{votes} / 10</Votes>
-        <Overview>{overview}</Overview>
+        <Votes>⭐️{votes} / 10</Votes>
+        <Overview>
+          {overview.length > 100 ? `${overview.slice(0, 100)}...` : overview}
+        </Overview>
+        <TouchableOpacity>
+          <Button>
+            <ButtonText>View Details</ButtonText>
+          </Button>
+        </TouchableOpacity>
       </Data>
     </Content>
   </Container>

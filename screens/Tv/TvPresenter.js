39444ae90e5f2830fe73417425688b4a7b71b330
SeungsooChoi <tmdtn5389@gmail.com>
2021-08-12 16:53:21 +0900

    Feat : Tv Done

--- screens/Tv/TvPresenter.js
+++ screens/Tv/TvPresenter.js
@@ -3,12 +3,14 @@ import ScrollContainer from "../../components/ScrollContainer";
 import HorizontalSlider from "../../components/HorizontalSlider";
 import Vertical from "../../components/Vertical";
 import styled from "styled-components/native";
+import List from "../../components/List";
+import Horizontal from "../../components/Horizontal";
 
 const Container = styled.View`
   margin-top: 30px;
 `;
 
-export default ({ loading, popular, topRated }) => (
+export default ({ loading, popular, topRated, today }) => (
   <ScrollContainer loading={loading}>
     <Container>
       <HorizontalSlider title="Popular Shows">
@@ -33,6 +35,17 @@ export default ({ loading, popular, topRated }) => (
           />
         ))}
       </HorizontalSlider>
+      <List title="Airing Today">
+        {today.map((show) => (
+          <Horizontal
+            key={show.id}
+            id={show.id}
+            title={show.name}
+            poster={show.poster_path || ""}
+            overview={show.overview}
+          />
+        ))}
+      </List>
     </Container>
   </ScrollContainer>
 );

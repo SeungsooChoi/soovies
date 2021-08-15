f7361d840c6c2f5acb9a9373c7516abd547a6bd9
SeungsooChoi <tmdtn5389@gmail.com>
2021-08-15 21:50:52 +0900

    Feat : test

--- components/Search/Input.js
+++ components/Search/Input.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 
 const TextInput = styled.TextInput`
   background-color: white;
-  font-size: 20px;
 `;
 
 const Input = ({ placeholder, value, onChange, onSubmit }) => (
